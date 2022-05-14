b7ce460757fd9f619452f098dbfb799aef973979
Alex Locke <alexhlocke@gmail.com>
2022-05-14 15:58:26 -0700

    Picasso is now functional and loading images

--- app/src/main/java/com/example/a438_group7_project3/HomeActivity.java
+++ app/src/main/java/com/example/a438_group7_project3/HomeActivity.java
@@ -2,13 +2,42 @@ package com.example.a438_group7_project3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageView mainImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mainImageView = findViewById(R.id.imageView);
+        String url = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.get()
+                .load(url)
+                .resize(400,400)
+                .centerCrop()
+                .error(R.drawable.captionit_logo_black_crop)
+                .into(mainImageView);
+
+
+//        Picasso picasso = new Picasso.Builder(this)
+//                .listener(new Picasso.Listener() {
+//                    @Override
+//                    public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+//                        Log.d("picasso", exception.toString());
+//                    }
+//                })
+//                .build();
+//
+//        picasso.load("https://i.imgur.com/DvpvklR.png")
+//                .into(mainImageView);
     }
 }
--- app/src/main/java/com/example/a438_group7_project3/MainActivity.java
+++ app/src/main/java/com/example/a438_group7_project3/MainActivity.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button toLoginActivity;
     private Button toCreateAccount;
+    private ImageView mainImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
         toLoginActivity = findViewById(R.id.buttonLogin);
         toCreateAccount = findViewById(R.id.createAccountButton);
+        mainImageView = findViewById(R.id.imageView);
+
+        //Load image into imageView
+        String url = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.get()
+                .load(url)
+                .resize(400,400)
+                .centerCrop()
+                .error(R.drawable.captionit_logo_black_crop)
+                .into(mainImageView);
 
         toLoginActivity.setOnClickListener(new View.OnClickListener() {
             @Override
